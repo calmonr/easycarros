b4a7dbfaec6187fabb72b4796c2e411b5b66ea3a
Calmon Ribeiro <calmonrib@gmail.com>
2020-08-19 01:00:36 -0300

    feat: jwt protection middleware

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,16 +1,15 @@
-import { sign } from 'jsonwebtoken'
+import { sign, Algorithm } from 'jsonwebtoken'
 import ms from 'ms'
 
 const { APPLICATION_KEY } = process.env
+
+export const algorithm: Algorithm = 'HS256'
 
 export const generateJWT = (
   userId: number
 ): { token: string; expiresIn: number } => {
   const expiresIn = ms('1h')
-  const token = sign({ userId }, APPLICATION_KEY, { expiresIn })
+  const token = sign({ userId }, APPLICATION_KEY, { algorithm, expiresIn })
 
-  return {
-    expiresIn,
-    token
-  }
+  return { expiresIn, token }
 }
